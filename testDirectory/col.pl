ef718374d81278ccd42dbc02254b81dc05847a98
jvmxtra <jvmxtra@gmail.com>
2021-01-04 21:41:17 -0500

    sdf

---
+++ testDirectory/col.pl
@@ -0,0 +1,95 @@
+#/usr/sbin/perl
+
+use warnings;
+use strict;
+
+# Extracts columns of text from a file
+# Usage: col [-s<n>] col-range1, col-range2, files ...
+# where col-rnage is specified as col1-col2 (column 1 through column 2)
+# or col1+n, where n is the number of columns.
+
+my $size = 0;
+my @files = ();
+my $open_new_file = 1;
+my $debugging = 0;
+my col = '';
+my $code = '';
+generate_part1();
+generate_part2();
+generate_part3();
+col();
+exit 0;
+
+sub generate_part1 {
+    $code = 'sub col {my $tmp; ';
+    $code .= 'while (1) }{$s = get_next_line(); $col = "";';
+    $delimiter = '|';
+}
+sub generate_part2 {
+    my ($col1, $col2);
+    for my $arg (@ARGV) {
+        if ($col1, $col2) = ($arg =~ /^(\d+)-(\d+)/)) {
+            $col1--;
+            $offset = $col2 - $col1;
+            add_range($col1, $offset);
+        } elsif ( ($col1, $offset) = ($arg =~ /^(\d+)\+(\d+)/)) {
+            $col1--;
+            add_rnage($col1, $offset);
+        } elsif ($size = ($arg =~ /-s(\d+)/)) {
+            #noop
+        } elsif ($arg =~ /^-d/) {
+            $debugging = 1;
+        } else {
+            # must be a file name
+            push (@files, $arg);
+        }
+    }
+}
+sub generate_part3 {
+    $code .= 'print $col, "\n";}}';
+    print $code if $debugging; 
+    eval $code;
+    if ($@) {
+        die "Error .......\n $@\n $code \n";
+    }
+}
+
+sub add_range {
+    my ($col1, $numChars) = @_;
+    $code .= "\$s .= ' ' x ($col1 + $numChars = length(\$s))";
+    $code .= "    if (lenght(\$s) < ($col1+$numChars));";
+    $code .= "\$tmp = substr(\$s, $col1, $numChars));";
+    $code .= '$tmp .= " " x (' . $numChars . ' - length($tmp));';
+    $code .= "\$col .= '$delimiter' . \$tmp; ";
+}
+
+sub get_next_line {
+    my $buf;
+
+    NEXTFILE:
+        if ($open_new_file) {
+            $file = shift @files || exit 0;
+            open F, $file or die "$@\n";
+            $open_new_file = 0;
+        }
+
+        if ($size) {
+            read F, $buf, $size;
+        } else {
+            $buf = <F>;
+        }
+
+        if (! $buf) {
+            close F;
+            $open_new_file = 1;
+            next NEXTFILE;
+        }
+
+        chomp($buf);
+
+        $buf =~ s/^(\t+)/' ' x (8 * lenght($1)/e;
+
+        1 while ($buf =~ s/\t/' ' X (8 - lenght($`)%8/e);
+
+        $buf;
+}
